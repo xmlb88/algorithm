dfdca681af62c21ca186561d5804d4f2e742c15c
xmlb_company <xmlb@gmail.com>
2020-09-19 17:52:32 +0800

    longestCommonSubsequence

--- leetcode/1143.longestCommonSubsequence.string_3.cpp
+++ leetcode/1143.longestCommonSubsequence.string_3.cpp
@@ -4,19 +4,63 @@ using namespace std;
 
 // 递归
 int longestCommonSubsequence(string text1, string text2) {
-
+    int m = text1.length();
+    int n = text2.length();
+    return dp(text1, text2, m - 1, n - 1);
 }
 
 int dp(string text1, string text2, int i, int j) {
-    if (i == -1)
+    // base case
+    if (i == -1 || j == -1) {
+        return 0;
+    }
+
+    if (text1[i] == text2[j]) {
+        return dp(text1, text2, i - 1, j - 1) + 1;
+    } else {
+        return max(dp(text1, text2, i - 1, j), dp(text1, text2, i, j - 1));
+    }
 }
 
 
+// 备忘录
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.length();
+    int n = text2.length();
+    vector<vector<int>> memo(m, vector<int> (n, -1));
+    return dp(text1, text2, m - 1, n - 1, memo);
+}
 
+int dp(string text1, string text2, int i, int j, vector<vector<int>>& memo) {
+    // base case
+    if (i == -1 || j == -1) return 0;
 
+    if (memo[i][j] != -1) return memo[i][j];
+    if (text1[i] == text2[j]) {
+        memo[i][j] = dp(text1, text2, i - 1, j - 1, memo) + 1;
+    } else {
+        memo[i][j] = max(dp(text1, text2, i, j - 1, memo), dp(text1, text2, i - 1, j, memo));
+    }
+
+    return memo[i][j];
+}
+
+// 超时
+// "fcvafurqjylclorwfoladwfqzkbebslwnmpmlkbezkxoncvwhstwzwpqxqtyxozkpgtgtsjobujezgrkvevklmludgtyrmjaxyputqbyxqvupojutsjwlwluzsbmvyxifqtglwvcnkfsfglwjwrmtyxmdgjifyjwrsnenuvsdedsbqdovwzsdghclcdexmtsbexwrszihcpibwpidixmpmxshwzmjgtadmtkxqfkrsdqjcrmxkbkfoncrcvoxuvcdytajgfwrcxivixanuzerebuzklyhezevonqdsrkzetsrgfgxibqpmfuxcrinetyzkvudghgrytsvwzkjulmhanankxqfihenuhmfsfkfepibkjmzybmlkzozmluvybyzsleludsxkpinizoraxonmhwtkfkhudizepyzijafqlepcbihofepmjqtgrsxorunshgpazovuhktatmlcfklafivivefyfubunszyvarcrkpsnglkduzaxqrerkvcnmrurkhkpargvcxefovwtapedaluhclmzynebczodwropwdenqxmrutuhehadyfspcpuxyzodifqdqzgbwhodcjonypyjwbwxepcpujerkrelunstebopkncdazexsbezmhynizsvarafwfmnclerafejgnizcbsrcvcnwrolofyzulcxaxqjqzunedidulspslebifinqrchyvapkzmzwbwjgbyrqhqpolwjijmzyduzerqnadapudmrazmzadstozytonuzarizszubkzkhenaxivytmjqjgvgzwpgxefatetoncjgjsdilmvgtgpgbibexwnexstipkjylalqnupexytkradwxmlmhsnmzuxcdkfkxyfgrmfqtajatgjctenqhkvyrgvapctqtyrufcdobibizihuhsrsterozotytubefutaxcjarknynetipehoduxyjstufwvkvwvwnuletybmrczgtmxctuny"
+// "nohgdazargvalupetizezqpklktojqtqdivcpsfgjopaxwbkvujilqbclehulatshehmjqhyfkpcfwxovajkvankjkvevgdovazmbgtqfwvejczsnmbchkdibstklkxarwjqbqxwvixavkhylqvghqpifijohudenozotejoxavkfkzcdqnoxydynavwdylwhatslyrwlejwdwrmpevmtwpahatwlaxmjmdgrebmfyngdcbmbgjcvqpcbadujkxaxujudmbejcrevuvcdobolcbstifedcvmngnqhudixgzktcdqngxmruhcxqxypwhahobudelivgvynefkjqdyvalmvudcdivmhghqrelurodwdsvuzmjixgdexonwjczghalsjopixsrwjixuzmjgxydqnipelgrivkzkxgjchibgnqbknstspujwdydszohqjsfuzstyjgnwhsrebmlwzkzijgnmnczmrehspihspyfedabotwvwxwpspypctizyhcxypqzctwlspszonsrmnyvmhsvqtkbyhmhwjmvazaviruzqxmbczaxmtqjexmdudypovkjklynktahupanujylylgrajozobsbwpwtohkfsxeverqxylwdwtojoxydepybavwhgdehafurqtcxqhuhkdwxkdojipolctcvcrsvczcxedglgrejerqdgrsvsxgjodajatsnixutihwpivihadqdotsvyrkxehodybapwlsjexixgponcxifijchejoxgxebmbclczqvkfuzgxsbshqvgfcraxytaxeviryhexmvqjybizivyjanwxmpojgxgbyhcruvqpafwjslkbohqlknkdqjixsfsdurgbsvclmrcrcnulinqvcdqhcvwdaxgvafwravunurqvizqtozuxinytafopmhchmxsxgfanetmdcjalmrolejidylkjktunqhkxchyjmpkvsfgnybsjedmzkrkhwryzan"
+
+// dp数组迭代
 int longestCommonSubsequence(string text1, string text2) {
     int m = text1.length();
     int n = text2.length();
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (text1[i - 1] == text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[m][n];
 }
