993bb8d9b4abaa7effa2892dc8b0012a3ae46493
xmlb_company <xmlb@gmail.com>
2020-11-18 18:26:55 +0800

    Sales_data

---
+++ c++primer/chapter-7/Sales_data.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Sales_data {
+public:
+    string isbn() const { return bookNo; }
+    Sales_data& combine(const Sales_data&);
+    double avg_price() const;
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data add(const Sales_data&, const Sales_data&);
+ostream &print(ostream&, const Sales_data&);
+istream &read(istream&, Sales_data&);
---
+++ c++primer/chapter-7/avg_price.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Sales_data.cc"
+using namespace std;
+
+int main() {
+    Sales_data total;
+    if (read(cin, total)) {
+        Sales_data trans;
+        while (read(cin, trans)) {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    } else {
+        cerr << "No data?!" << endl;
+    }
+}
