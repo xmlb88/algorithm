8c480bc55e212cf6466035e952e2428d699b97d0
xmlb_company <xmlb@gmail.com>
2021-05-24 20:58:20 +0800

    want sleep

--- leetcode/DFS/664.strangePrinter.cpp
+++ leetcode/DFS/664.strangePrinter.cpp
@@ -4,8 +4,26 @@
 using namespace std;
 
 int strangePrinter(string s) {
-    // dp[i][j]表示从i开始到j的最小次数
-    // dp[i][j] = 
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n, 1));
+
+    for (int i = n - 1; i >= 0; --i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (s[i] == s[j]) {
+                dp[i][j] = dp[i][j - 1];
+            } else {
+                int minn = INT_MAX;
+                for (int k = i; k < j; k++) {
+                    minn = min(minn, dp[i][k] + dp[k + 1][j]);
+                }
+                dp[i][j] = minn;
+            }
+        }
+    }
+    return dp[0][n - 1];
 }
+
+// dp[i][j] = dp[i + 1][j - 1] | dp[i + 1][j] | dp[i][j - 1]
+// tbgtgb
 
 // aaabbbbssffzzaa
